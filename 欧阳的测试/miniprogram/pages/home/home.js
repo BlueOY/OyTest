1b00cc4c36ce7df24dfa0f1f69aafebe75acc943
oyzd <328082876@qq.com>
2020-05-09 21:49:51 +0800

    改版成网店

--- 欧阳的测试/miniprogram/pages/home/home.js
+++ 欧阳的测试/miniprogram/pages/home/home.js
@@ -1,11 +1,20 @@
-// miniprogram/pages/home.js
+// miniprogram/pages/info/info.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
 
+    indicatorDots: true,
+    vertical: false,
+    autoplay: false,
+    circular: false,
+    interval: 2000,
+    duration: 500,
+    previousMargin: 0,
+    nextMargin: 0
   },
 
   /**
--- 欧阳的测试/miniprogram/pages/info/info.js
+++ 欧阳的测试/miniprogram/pages/info/info.js
@@ -5,16 +5,6 @@ Page({
    * 页面的初始数据
    */
   data: {
-    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
-
-    indicatorDots: true,
-    vertical: false,
-    autoplay: false,
-    circular: false,
-    interval: 2000,
-    duration: 500,
-    previousMargin: 0,
-    nextMargin: 0
   },
 
   /**
