62e9771551706bc650fb04ca4e5f6da37b0d1b56
oyzd <328082876@qq.com>
2020-05-10 12:48:52 +0800

    购物车静态页面及部分页面修改

--- 欧阳的测试/miniprogram/pages/cart/cart.js
+++ 欧阳的测试/miniprogram/pages/cart/cart.js
@@ -1,18 +1,22 @@
-// miniprogram/pages/order/order.js
+// miniprogram/pages/cart/cart.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    scroll_height: 0,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let windowHeight = wx.getSystemInfoSync().windowHeight // 屏幕的高度
+    let windowWidth = wx.getSystemInfoSync().windowWidth // 屏幕的宽度
+    this.setData({
+      scroll_height: windowHeight * 750 / windowWidth - 80 - 30
+    })
   },
 
   /**
