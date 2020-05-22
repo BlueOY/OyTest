b5484a52becd744ec4b1aeecca03f5731a3a3123
oyzd <328082876@qq.com>
2020-05-22 22:21:14 +0800

    完善调用云函数

--- WeChatAppletTest/src/main/java/com/trajectory/wechatapplettest/controller/IndexController.java
+++ WeChatAppletTest/src/main/java/com/trajectory/wechatapplettest/controller/IndexController.java
@@ -3,12 +3,11 @@ package com.trajectory.wechatapplettest.controller;
 import com.alibaba.fastjson.JSON;
 import com.trajectory.wechatapplettest.utils.StaticClass;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 //证明是controller层并且返回页面
@@ -17,13 +16,14 @@ public class IndexController {
 
     @ResponseBody   //返回数据不返回页面
     @RequestMapping("/")
-    public String home() {
+    public String index() {
         return "Hello World!";
     }
 
     @ResponseBody
     @RequestMapping(value = "invokeCloudFunction")
-    public String invokeCloudFunction(Map<Object, Object> param, String functionName) {
+    public String invokeCloudFunction(String functionName, String param) {
+        System.out.println("functionName="+functionName);
         System.out.println("param="+param);
 
         if(StaticClass.access_token==null){
@@ -34,9 +34,15 @@ public class IndexController {
             StaticClass.getToken();
         }
         // 访问云函数
-        String POSTBODY = JSON.toJSONString(param);
-        String res = StaticClass.testFunction(functionName, POSTBODY);
+        String res = StaticClass.testFunction(functionName, param);
         return res;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "test", method = RequestMethod.POST)
+    public String test(@RequestBody Map<String, String> map) {
+        System.out.println("param="+map);
+        return "true";
     }
 
 }
--- WeChatAppletTest/src/main/resources/static/script/common.js
+++ WeChatAppletTest/src/main/resources/static/script/common.js
@@ -3,35 +3,62 @@ Array.prototype.remove = function(from, to) {
   this.length = from < 0 ? this.length + from : from;
   return this.push.apply(this, rest);
 };
-function httpGet(url, data, success){
-  $.ajax({
-    type: 'GET',
-    url: url,
-    data: data,
-    // dataType: "json",
-    success: function(data){
-        if(success){
-          success(data)
-        }
-    },
-    error: function(){
-        alert("error");
-    }
-  });
-}
-function httpPost(url, data, success){
-  $.ajax({
-    type: 'POST',
-    url: url,
-    data: data,
-    // dataType: "json",
-    success: function(data){
-        if(success){
-          success(data)
-        }
-    },
-    error: function(){
-        alert("error");
-    }
-  });
-}
+(function(window){
+    var u = {};
+    u.httpGet = function(url, data, success){
+        $.ajax({
+            type: 'GET',
+            url: url,
+            data: data,
+            // dataType: "json",
+            success: function(data){
+                if(success){
+                    success(data)
+                }
+            },
+            error: function(){
+                alert("error");
+            }
+        });
+    };
+    u.httpPost = function(url, data, success){
+        $.ajax({
+            type: 'POST',
+            url: url,
+            data: data,
+            // dataType: "json",
+            // contentType : "application/json",
+            success: function(data){
+                if(success){
+                    success(data)
+                }
+            },
+            error: function(){
+                alert("error");
+            }
+        });
+    };
+    u.invokeCloudFunction = function(name, param, success){
+        var url = "/invokeCloudFunction";
+        var data = {
+            functionName: name,
+            param: JSON.stringify(param),
+        };
+        $.ajax({
+            type: 'POST',
+            url: url,
+            data: data,
+            // dataType: "json",
+            // contentType : "application/json",
+            success: function(data){
+                if(success){
+                    success(data)
+                }
+            },
+            error: function(){
+                alert("error");
+            }
+        });
+    };
+    window.$Common = u;
+})(window);
